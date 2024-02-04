33d3a00a805ed1ed1d5aacd5a1c9804a1aad0b2c
arif853 <arifhossen853@gmail.com>
2024-02-04 10:41:51 +0600

    folder change commit

--- resources/views/livewire/select-size-component.blade.php
+++
@@ -1,9 +0,0 @@
-<ul class="list-filter size-filter font-small">
-
-    @foreach ($sizes as $size)
-    <li class="{{ $size->id == session()->get('product_size')  ? 'active' : '' }}">
-        <a  href="#"  wire:click.prevent="selectSize({{ $size->id }})" class="{{ $size->size_name == session()->get('product_size')  ? 'active' : '' }}" >{{$size->size}}</a></li>
-    {{-- wire:click.prevent="selectSize({{ $size->id }})" wire:key="{{ $size->id }}" --}}
-    @endforeach
-      <li><a class="btn size-button ml-15" href="#">Size Chart</a></li>
-   </ul>
--- resources/views/livewire/user-transactions-component.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="tab-pane fade profile-header" id="pills-transaction" role="tabpanel" aria-labelledby="pills-transaction-tab">
-    <h2>Transactions</h2>
-</div>
