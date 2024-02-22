00e21cd7f9730fe2186376c6220218738f909c1e
arif853 <arifhossen853@gmail.com>
2024-02-22 14:48:48 +0600

    email template

--- app/Mail/AdminMail.php
+++ app/Mail/AdminMail.php
@@ -34,7 +34,7 @@ class AdminMail extends Mailable
 
 
     public function build(){
-        return $this->view('admin.email.adminmail',[
+        return $this->view('admin.email.mail',[
             'order' => $this->data
         ]);
     }
@@ -45,7 +45,7 @@ class AdminMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'admin.email.adminmail',
+            view: 'admin.email.mail',
         );
     }
 
