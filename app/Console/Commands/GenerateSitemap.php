52904c7185f4e87902f352189a942081f524644d
Ali Sami <alisamicse320@gmail.com>
2024-03-24 16:29:09 +0600

    dynamic sitemap

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -26,6 +26,7 @@ class GenerateSitemap extends Command
      * Execute the console command.
      */
     public function handle()
+    
     {
         $sitemap = Sitemap::create();
          // Static pages
